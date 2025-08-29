0901bd0c830bb2af79765ac33a508f353403b766
zhiyan114 <zhiyan114@furries.dev>
2025-08-28 22:06:29 -0400

    Sentry Handler (internal tool)

---
+++ sentry.php
@@ -0,0 +1,16 @@
+// Add require_once /var/www/html/sentry.php;
+// Error Logger toolkit
+
+<?php
+require_once vendor/autoload.php;
+
+\Sentry\init([
+  'dsn' => 'https://b04e1712f35bd482f7705e163822dec5@o125145.ingest.us.sentry.io/4509924680859648',
+  'traces_sample_rate' => 1.0,
+  'profiles_sample_rate' => 1.0,
+  'enable_logs' => true,
+]);
+?>
+<script>
+        console.log("~PHP Sentry Enabled~");
+</script>
