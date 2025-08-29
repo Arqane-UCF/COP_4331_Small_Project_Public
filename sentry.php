d3099ecd23a1d76821e1898c3228d196fa5de268
zhiyan114 <zhiyan114@furries.dev>
2025-08-28 22:34:39 -0400

    Fix :p

--- sentry.php
+++ sentry.php
@@ -1,8 +1,8 @@
-// Add require_once /var/www/html/sentry.php;
+<?php
+// Add require_once "/var/www/html/sentry.php";
 // Error Logger toolkit
 
-<?php
-require_once vendor/autoload.php;
+require_once "vendor/autoload.php";
 
 \Sentry\init([
   'dsn' => 'https://b04e1712f35bd482f7705e163822dec5@o125145.ingest.us.sentry.io/4509924680859648',
