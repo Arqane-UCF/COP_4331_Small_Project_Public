d63ed75c344d31758dd24d9d0c9a429c3e07be22
zhiyan114 <zhiyan114@furries.dev>
2025-08-30 00:24:20 -0400

    Added front-end sentrying as well

--- loader.php
+++ loader.php
@@ -10,7 +10,7 @@ $dotenv->load();
 
 // Configure Sentry
 $S_DSN = getenv('SENTRY_DSN');
-if($S_DSN !== false)
+if($S_DSN !== false) {
     \Sentry\init([
         'dsn' => $S_DSN,
         'send_default_pii' => true,
@@ -18,3 +18,33 @@ if($S_DSN !== false)
         'profiles_sample_rate' => 1.0,
         'enable_logs' => true,
     ]);
+?>
+    <!-- Frontend Sentry loader stuff -->
+    <script src="/scripts/sentry.js" data-lazy="no"></script>
+    <script>
+        if(window.Sentry) {
+            Sentry.init({
+                dsn: "<?php echo $S_DSN; ?>",
+                integrations: [
+                    Sentry.browserTracingIntegration(),
+                    Sentry.replayIntegration()
+                ],
+                tracesSampleRate: 1.0, // My account has the budget LOL
+                replaysSessionSampleRate: 0.0, // Useless
+                replaysOnErrorSampleRate: 1.0, // Might have enough budget
+                sendDefaultPii: true,
+                enableLogs: true,
+                tunnel: "/tunnel.php"
+                ignoreErrors: [
+                    "jQuery",
+                    "Failed to fetch",
+                    "EADDRINUSE"
+                ]
+            });
+            console.log("Sentry Loaded Done ^w^");
+        }
+
+    </script>
+<?php
+}
+?>
